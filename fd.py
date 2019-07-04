OSBI and EQ Rotation Corrected

fd.py
@@ -7,41 +7,50 @@ from math import sin, cos, tan, atan, pow, exp, sqrt, pi
 import scipy.integrate as integrate
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+                 
+# iso = IsoOSBIModel(1.0, 50.0, 0.0, 1.0, 0.0, 1, 0.1, 1, 1, 1, 0.85, 0.4, 0.5, 0.05, 100, 100, 0.8, 4140, 4)
+# print(iso)
 
-iso = IsoOSBIModel(rmbm = 1.0, tbx = 50.0, zetabx = 0.0, rtytxb = 1.0, rzyzxb = 0.0, typevf = 1, mu0 = 0.1, alpha0 = 1, alpha1 = 1, nu = 1, umax = 0.85, D = 0.4, ecc = 0.5, rmrm = 0.05, kg = 100, cg = 100, dg = 1000)
+# drb = 10  # -0.5869 # -0.6283
 
-drb = 10  # -0.5869 # -0.6283
+# xb = 0.2 # 0.41500097
+# yb = 0.2 # 0.41500097
 
-xb = 0.2 # 0.41500097
-yb = 0.2 # 0.41500097
-
-M = 1000
-mr = iso.rmrm*M
+# M = 4140 + 4140
 
 
-drb = sqrt(xb**2.0 + yb**2.0)
-print('Resultant displacement = %8.4f m'%(drb))
 
-print('Major radius of osbi (a0) = %8.4f'%(iso.a0))
-print('Minor radius of osbi (b0) = %8.4f'%(iso.b0))
+# drb = sqrt(xb**2.0 + yb**2.0)
+# print('Resultant displacement = %8.4f m'%(drb))
 
-theta_d0 = np.interp(drb, iso.xbtab, iso.ttab)
-print('Rotation angle (theta_d0) = %8.4f rad'%theta_d0)
+# print('Major radius of osbi (a0) = %8.4f'%(iso.a0))
+# print('Minor radius of osbi (b0) = %8.4f'%(iso.b0))
 
-theta_r_d0 = atan((iso.b0/iso.a0)*tan(theta_d0))
-print('Eccentric angle (theta_r_d0) = %8.4f rad'%(theta_r_d0))
+# theta_d0 = np.interp(drb, iso.xbtab, iso.ttab)
+# print('Rotation angle (theta_d0) = %8.4f rad'%theta_d0)
 
-c_d0 = iso.a0*sin(theta_d0)*cos(theta_r_d0) - iso.b0*cos(theta_d0)*sin(theta_r_d0)
-print('Horizontal offset (c_d0) = %8.4f m'%(c_d0))
+# theta_r_d0 = atan((iso.b0/iso.a0)*tan(theta_d0))
+# print('Eccentric angle (theta_r_d0) = %8.4f rad'%(theta_r_d0))
 
-p_d0 = iso.a0*sin(theta_d0)*sin(theta_r_d0) + iso.b0*cos(theta_d0)*cos(theta_r_d0)
-print('Vertical offset (p_d0) = %8.4f m'%(p_d0))
+# c_d0 = iso.a0*sin(theta_d0)*cos(theta_r_d0) - iso.b0*cos(theta_d0)*sin(theta_r_d0)
+# print('Horizontal offset (c_d0) = %8.4f m'%(c_d0))
 
-fs1x, fs1y = fs1fixed(M, mr, c_d0, p_d0, drb, yb, xb)
-print("Mass of structure (M) = %8.4f kg"%(M))
-print("Mass of all osbi balls (mr) = %8.4f kg"%(mr))
-print("Restoring force in X-direction (fs1x) = %8.4f N"%(fs1x))
-print("Restoring force in Y-direction (fs1y) = %8.4f N"%(fs1y))
+# p_d0 = iso.a0*sin(theta_d0)*sin(theta_r_d0) + iso.b0*cos(theta_d0)*cos(theta_r_d0)
+# print('Vertical offset (p_d0) = %8.4f m'%(p_d0))
+
+# fs1x, fs1y = fs1fixed(M, iso.Mr, c_d0, p_d0, drb, yb, xb)
+# print("Mass of structure (M) = %8.4f kg"%(M))
+# print("Mass of all osbi balls (mr) = %8.4f kg"%(iso.Mr))
+# print("Restoring force in X-direction (fs1x) = %8.4f N"%(fs1x))
+# print("Restoring force in Y-direction (fs1y) = %8.4f N"%(fs1y))
+
+
+# print(np.interp(0.628319, iso.xbtab, iso.ttab)*180/pi)
+# plt.plot(iso.xbtab, iso.ttab*180/pi)
+# plt.xlabel('xbtab')
+# plt.ylabel('theta_0_or_r')
+# plt.show()
 
 
 
@@ -69,3 +78,162 @@ print("Restoring force in Y-direction (fs1y) = %8.4f N"%(fs1y))
 # fbfixed(mr, arg, phi_arg)
 # fs2(M, mr, mu, y_b_d2)
 # fs2fixed(mu, M, mr)
+
+
+# drb = 10  # -0.5869 # -0.6283
+
+# xb = 0.2 # 0.41500097
+# yb = 0.2 # 0.41500097
+
+# M = 4140 + 4140
+
+
+
+# drb = sqrt(xb**2.0 + yb**2.0)
+# print('Resultant displacement = %8.4f m'%(drb))
+
+# print('Major radius of osbi (a0) = %8.4f'%(iso.a0))
+# print('Minor radius of osbi (b0) = %8.4f'%(iso.b0))
+
+
+rmbm = 1.0
+tbx = 50.0
+zetabx = 0.0 
+rtytxb = 1.0
+rzyzxb = 1.0
+typevf = 1
+mu0 = 0.01
+alpha0 = 1
+alpha1 = 1
+nu = 1
+umax = 0.85
+D = 0.4
+ecc = 0.4
+rmrm = 0.05
+kg = 100
+cg = 100
+dg = 0.8
+am = 4140/4
+niso = 1
+
+iso = IsoOSBIModel(rmbm, tbx, zetabx, rtytxb, rzyzxb, typevf, mu0, alpha0, alpha1, nu, umax, D, ecc, rmrm, kg, cg, dg, am, niso)
+# print(iso)
+
+# trd0 = 90
+# print('Theta_r = %6.3f deg'%(trd0))
+# trd0 = trd0*pi/180.0
+
+# td0 = atan(iso.a0/iso.b0*tan(trd0))
+# print('Theta = %6.3f deg'%(td0*180.0/pi))
+
+# c = iso.a0*sin(td0)*cos(trd0) - iso.b0*cos(td0)*sin(trd0) # with modification
+# print('c = %6.3f m'%(c))
+
+# p = iso.a0*sin(td0)*sin(trd0) + iso.b0*cos(td0)*cos(trd0)
+# print('p = %6.3f'%(p))
+
+# fun1 = lambda x: iso.a0*sqrt(1-pow(iso.ecc*sin(x), 2.0)) # with modification
+
+# I = integrate.quad(fun1, 0, td0)
+# print('I = %6.3f'%(I[0]))
+
+# xb = 2.0*(I[0] - c)
+# print('xb = %6.3f'%(xb))
+
+# If = integrate.quad(fun1, 0, pi/2)
+
+# print('Half-way mark = %6.3f'%(I[0]/If[0]))
+
+Dt = 2.0*iso.a0
+ro = 3400
+mr = pi/6.0*ro*pow(Dt, 3.0)*sqrt(1 - pow(iso.ecc, 2.0))
+M = 1*4140.0/4*5
+rmrM = mr/M
+g = 9.81
+
+
+t_d0 = lambda tr: atan((iso.a0/iso.b0)*tan(tr))
+T_d0 = lambda tr: atan((1.0/sqrt(1 - pow(iso.ecc, 2.0)))*tan(tr))
+
+F = lambda tr : iso.a0*sqrt(1-pow(iso.ecc*sin(tr), 2.0))
+FF = lambda tr: sqrt(1-pow(iso.ecc*sin(T_d0(tr)), 2.0))
+
+c_d0 = lambda tr : iso.a0*sin(atan(iso.a0/iso.b0*tan(tr)))*cos(tr) - iso.b0*cos(atan(iso.a0/iso.b0*tan(tr)))*sin(tr)
+C_d0 = lambda tr: sin(T_d0(tr))*cos(tr) - sqrt(1 - pow(iso.ecc, 2.0))*cos(T_d0)*sin(tr)
+
+p_d0 = lambda tr : iso.a0*sin(atan(iso.a0/iso.b0*tan(tr)))*sin(tr) + iso.b0*cos(atan(iso.a0/iso.b0*tan(tr)))*cos(tr)
+P_d0 = lambda tr: sin(T_d0(tr))*sin(tr) + sqrt(1 - pow(iso.ecc, 2.0))*cos(T_d0)*cos(tr)
+
+tr_d1 = lambda tr : (iso.b0/iso.a0)*pow(cos(tr)/cos(t_d0(tr)),2.0) #sin(2*tr)/sin(2*atan(iso.a0/iso.b0*tan(tr)))
+Tr_d1 = lambda tr : sqrt(1 - pow(iso.ecc, 2.0))*pow(cos(tr)/cos(t_d0(tr)),2.0)
+
+c_d1 = lambda tr : (iso.a0*cos(atan(iso.a0/iso.b0*tan(tr)))*cos(tr) + iso.b0*sin(atan(iso.a0/iso.b0*tan(tr)))*sin(tr)) - p_d0(tr)*tr_d1(tr)
+C_d1 = lambda tr : cos(T_d0(tr))*cos(tr) + sqrt(1 - pow(iso.ecc, 2.0))*sin(T_d0(tr))*sin(tr) - P_d0(tr)*Tr_d1(tr)
+
+p_d1 = lambda tr : (iso.a0*cos(atan(iso.a0/iso.b0*tan(tr)))*sin(tr) - iso.b0*sin(atan(iso.a0/iso.b0*tan(tr)))*cos(tr)) + c_d0(tr)*tr_d1(tr)
+P_d1 = lambda tr : cos(T_d0(tr))*sin(tr) - sqrt(1 - pow(iso.ecc, 2.0))*sin(T_d0(tr))*cos(tr) + C_d0(tr)*Tr_d1(tr)
+
+
+
+
+ld0 = 0.0 # Lower limit of td0
+ud0 = pi/2 # Upper limit of td0
+tdiv = (ud0)/10000
+sizeTable = int((ud0 - ld0)/tdiv + 1)
+print(sizeTable)
+
+xbtab = np.zeros((sizeTable, ), dtype=np.dtype('d'), order ='F')
+kt = np.zeros((sizeTable, ), dtype=np.dtype('d'), order ='F')
+tr = np.zeros((sizeTable, ), dtype=np.dtype('d'))
+t0 = np.zeros((sizeTable, ), dtype=np.dtype('d'))
+
+
+# i = 0
+# for tr in np.arange(0.0,89.9999*pi/180,89.9999*pi/180/tdiv):
+    
+#     trs[i] = tr
+#     kt[i] = (M*g*(p_d0(tr)*c_d1(tr) - c_d0(tr)*p_d1(tr)))/(pow(p_d0(tr),2)*(2.0*tr_d1(tr)*F(tr) - 2*c_d1(tr)))
+#     itheta = integrate.quad(F, 0.0, t_d0(tr))
+#     xbtab[i] = 2.0*itheta[0] - 2.0*c_d0(tr)
+#     if i >= 1:
+#         if kt[i-1] >= 0 and kt[i] < 0: 
+#             Fb = (2*M + iso.Mr)*g*c_d0(tr)/p_d0(tr)
+#             print('kt = %8.3f N/m | xb = %8.3f m | tr = %8.3f deg'%(kt[i-1], xbtab[i-1], trs[i-1]*180/pi))
+#             print('kt = %8.3f N/m | xb = %8.3f m | tr = %8.3f deg'%(kt[i], xbtab[i], trs[i]*180/pi))
+#             print('Fb = %8.3f N'%Fb)
+#             break
+
+#     i = i+1
+
+i = 0
+
+for td0 in np.arange(ld0, ud0 + tdiv, tdiv):
+    t0[i] = td0
+    tr[i] = atan(iso.b0/iso.a0*tan(td0))
+    kt[i] = (M*g*(p_d0(tr[i])*c_d1(tr[i]) - c_d0(tr[i])*p_d1(tr[i])))/(pow(p_d0(tr[i]),2)*(2.0*tr_d1(tr[i])*F(tr[i]) - 2*c_d1(tr[i])))
+    itheta = integrate.quad(F, 0.0, td0)
+    xbtab[i] = 2.0*itheta[0] - 2.0*c_d0(tr[i])
+    if i >= 1:
+        if kt[i-1] >= 0 and kt[i] < 0: 
+            Fb = (2*M + iso.Mr)*g*c_d0(tr[i])/p_d0(tr[i])
+            print('kt = %8.3f N/m | xb = %8.3f m | tr = %8.3f deg'%(kt[i-1], xbtab[i-1], tr[i-1]*180/pi))
+            print('kt = %8.3f N/m | xb = %8.3f m | tr = %8.3f deg'%(kt[i], xbtab[i], tr[i]*180/pi))
+            print('Fb = %8.3f N'%Fb)
+    i = i+1
+print(i)
+print(tr, xbtab)
+
+plt.plot(t0*180/pi, xbtab, iso.ttab*180/pi, iso.xbtab)
+plt.xlabel('trs (deg)')
+plt.ylabel('xbtab (m)')
+plt.show()
+
+# itheta = integrate.quad(F, 0.0, t_d0(20*pi/180))
+# xbtab = 2.0*itheta[0] - 2.0*c_d0(20*pi/180)
+# print(itheta)
+# print(c_d0(20*pi/180))
+# print(xbtab)
+
+
+# fun1 = lambda x: sqrt(1 + pow((iso.b0/iso.a0)*((x*sqrt(pow(iso.a0, 2.0) - pow(x, 2.0)))/2.0 + (pow(iso.a0, 2.0)*np.sinh(x/iso.a0))/2.0), 2.0))
+# itheta = integrate.quad(fun1, 0.0, )

mhps.py
@@ -16,6 +16,7 @@ from mhps.isolator_l import addlinear_iso, simulator_linear, read_iso_l_var_para
 from mhps.isolator_pf import simulator_pf, read_iso_pf_var_param, IsoPFModel
 from mhps.isolator_boucwen import simulator_boucwen, read_iso_boucwen_var_param, IsoBoucWenModel, addboucwen_iso
 from mhps.isolator_osbi import simulator_osbi, read_iso_osbi_var_param, IsoOSBIModel
+from mhps.isolator_osbix import simulator_osbix
 from progressbar import progressbar
 from mhps.postprocessor import result_viewer, createfolder
 from pathlib import Path
@@ -181,6 +182,8 @@ def fixed_fn(const_param, var_param, earthquakes, knor, results_type, lxy, folde
 
             if ((i == 0) and (j==0)) or ((p_nst, p_tx1, p_rtytx, p_zeta != nst, tx1, rtytx, zeta)):
                 smx, skx, cdx, smy, sky, cdy = superstructure_propxy(nst, tx1, rtytx, am, ak, zeta, knor)
+                print(smx)
+                print(skx)
                 p_nst, p_tx1, p_rtytx, p_zeta = nst, tx1, rtytx, zeta
                 
             result, model = fixed_simulator(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, nst, smx[0:nst,0:nst], skx[0:nst,0:nst], cdx[0:nst,0:nst], smy[0:nst,0:nst], sky[0:nst,0:nst], cdy[0:nst,0:nst], screen)
@@ -206,7 +209,10 @@ def fixed_fn(const_param, var_param, earthquakes, knor, results_type, lxy, folde
                 peakmat = np.hstack((peakmat, peakvalues))
                 peakmathead += [s+'-EQ-'+str(result.eq_ref) for s in peakvaluesparamhead]
             if i == (total_eq - 1):
-                peakmat = pd.DataFrame(peakmat)
+                if total_param == 1:
+                    peakmat = pd.DataFrame(peakmat.reshape(-1, len(peakmat)))
+                else:
+                    peakmat = pd.DataFrame(peakmat)
                 # print(peakmat)
                 # print(peakvaluesparamhead)
                 peakmat.columns = peakmathead
@@ -404,7 +410,10 @@ def spectral(earthquakes, results_type, folder, damping, screen):
                 peakmat = np.hstack((peakmat, peakvalues))
                 peakmathead += [s+'-EQ-'+str(result.eq_ref) for s in peakvaluesparamhead]
             if i == (total_eq - 1):
-                peakmat = pd.DataFrame(peakmat)
+                if total_param == 1:
+                    peakmat = pd.DataFrame(peakmat.reshape(-1, len(peakmat)))
+                else:
+                    peakmat = pd.DataFrame(peakmat)
                 # print(peakmat)
                 # print(peakvaluesparamhead)
                 peakmat.columns = peakmathead
@@ -548,7 +557,10 @@ def biso_linear(const_param, var_param, iso_param, earthquakes, knor, results_ty
                 peakmat = np.hstack((peakmat, peakvalues))
                 peakmathead += [s+'-EQ-'+str(result.eq_ref) for s in peakvaluesparamhead]
             if i == (total_eq - 1):
-                peakmat = pd.DataFrame(peakmat)
+                if total_param == 1:
+                    peakmat = pd.DataFrame(peakmat.reshape(-1, len(peakmat)))
+                else:
+                    peakmat = pd.DataFrame(peakmat)
                 # print(peakmat)
                 # print(peakvaluesparamhead)
                 peakmat.columns = peakmathead
@@ -670,7 +682,10 @@ def biso_pf(const_param, var_param, iso_param, earthquakes, knor, results_type, 
 
         if peakvalues is not None:
             if i == 0:
-                peakmat = peakvalues
+                if total_param == 1:
+                    peakmat = pd.DataFrame(peakmat.reshape(-1, len(peakmat)))
+                else:
+                    peakmat = pd.DataFrame(peakmat)
                 peakmathead = [s+'-EQ-'+str(result.eq_ref) for s in peakvaluesparamhead]
             else:
                 peakmat = np.hstack((peakmat, peakvalues))
@@ -713,8 +728,8 @@ def biso_pf(const_param, var_param, iso_param, earthquakes, knor, results_type, 
 @click.option('--iso_param', '-vp',type=str, default= 'OSBI_VP.csv', help= 'File name containing variable parameters for linear base isolator')
 @click.option('--earthquakes','-eq', type=(str), default= "Excitations.csv", help= 'Earthquakes')
 @click.option('--knor', '-knor', type=int, default=1, help="Normalizing the superstructure for given time period. 1 for normalized and 0 for un-normalized.")
-@click.option('--results_type', '-r', type=str, default="g*, aa1, db, f*, paa1, pdb", help="Choice to select output results")
-@click.option('--folder', '-f', type=str, default="Result", help="Folder name to store result")
+@click.option('--results_type', '-r', type=str, default="g*, aa1, db, f*, ro*, sm*, paa1, pdb", help="Choice to select output results")
+@click.option('--folder', '-f', type=str, default="result", help="Folder name to store result")
 @click.option('--outputunits', type=list, default=['m/s2', 'cm/s', 'cm', 'kn', 'j'])
 @click.option('--lxy', '-lxy', type=int, default=0)
 @click.option('--screen/--no-screen', default=False)
@@ -770,7 +785,7 @@ def biso_osbi(const_param, var_param, iso_param, earthquakes, knor, results_type
         superstructure_param_generator = read_ss_var_param(var_param)
         
         # ISOLATOR VARIABLE PARAMETER SETUP
-        isolator_param_generator = read_iso_osbi_var_param(iso_param, am[0], 1.0)
+        isolator_param_generator = read_iso_osbi_var_param(iso_param, am[0], 4)
 
         for j in range(total_param):
             ijk, nst, tx1, zeta, rtytx = next(superstructure_param_generator)
@@ -795,6 +810,7 @@ def biso_osbi(const_param, var_param, iso_param, earthquakes, knor, results_type
                 else:
                     peakvalues = np.vstack((peakvalues, peakvaluesparam))
 
+
         if peakvalues is not None:
             if i == 0:
 
@@ -803,14 +819,16 @@ def biso_osbi(const_param, var_param, iso_param, earthquakes, knor, results_type
                 # This place is dedicated to add model info in peak results as in spectral
 
                 # 
-
                 peakmat = peakvalues
                 peakmathead = [s+'-EQ-'+str(result.eq_ref) for s in peakvaluesparamhead]
             else:
                 peakmat = np.hstack((peakmat, peakvalues))
                 peakmathead += [s+'-EQ-'+str(result.eq_ref) for s in peakvaluesparamhead]
             if i == (total_eq - 1):
-                peakmat = pd.DataFrame(peakmat)
+                if total_param == 1:
+                    peakmat = pd.DataFrame(peakmat.reshape(-1, len(peakmat)))
+                else:
+                    peakmat = pd.DataFrame(peakmat)
                 peakmat.columns = peakmathead
                 peakmat.to_csv(os.path.join("results", folder, "Peak.csv"), mode='w', sep=',', index=False)
         end_t = time.time()
@@ -933,7 +951,10 @@ def biso_boucwen(const_param, var_param, iso_param, earthquakes, knor, results_t
                 peakmat = np.hstack((peakmat, peakvalues))
                 peakmathead += [s+'-EQ-'+str(result.eq_ref) for s in peakvaluesparamhead]
             if i == (total_eq - 1):
-                peakmat = pd.DataFrame(peakmat)
+                if total_param == 1:
+                    peakmat = pd.DataFrame(peakmat.reshape(-1, len(peakmat)))
+                else:
+                    peakmat = pd.DataFrame(peakmat)
                 peakmat.columns = peakmathead
                 peakmat.to_csv(os.path.join("results", folder, "Peak.csv"), mode='w', sep=',', index=False)
         end_t = time.time()

mhps/isolator_osbix.py
@@ -34,8 +34,8 @@ def profile(fnc):
 
     return inner
 
-class IsoOSBIModel:   
-
+class IsoOSBIModel:    
+    
     def osbt2x(self): 
         default_values = get_default_param_values()
         tdiv = default_values['TDIV']
@@ -194,9 +194,10 @@ def fs1(M, rMrM, y_b_d2, c_d0, p_d0, drb, dyb, dxb):
         fs1y = 0.0
     else:
         fs1x = fs1r*dxb/drb
-        fs1y = fs1r*dyb/drb
+        fs1y = 0.0
 
     return fs1x, fs1y
+
 
 def fs1fixed(M, rMrM, c_d0, p_d0, drb, dyb, dxb):
 
@@ -207,8 +208,8 @@ def fs1fixed(M, rMrM, c_d0, p_d0, drb, dyb, dxb):
         fs1x = 0.0
         fs1y = 0.0
     else:
-        fs1x = fs1r*dxb/drb
-        fs1y = fs1r*dyb/drb
+        fs1x = fs1r
+        fs1y = 0.0
 
     return fs1x, fs1y
 
@@ -219,32 +220,32 @@ def fb(J, mr, drb, dyb, dxb, vrb, vyb, vxb, arg, ayg, axg, arb, ayb, axb, p_d0, 
         fbr1y = 0.0
     else:
         fbr1 = (1/2.0/p_d0)*(J*theta_r_dot2)
-        fbr1x = fbr1*axb/arb 
-        fbr1y = fbr1*ayb/arb
+        fbr1x = fbr1
+        fbr1y = 0.0
 
     if arg == 0:
         fbr2x = 0.0
         fbr2y = 0.0
     else:
         fbr2 = 0.5*mr*arg
-        fbr2x = fbr2*axg/arg 
-        fbr2y = fbr2*ayg/arg
+        fbr2x = fbr2
+        fbr2y = 0.0
 
     if arb == 0:
         fbr3x = 0.0
         fbr3y = 0.0
     else:
         fbr3 =  0.5*mr*0.5*arb
-        fbr3x = fbr3*axb/arb 
-        fbr3y = fbr3*ayb/arb
+        fbr3x = fbr3 
+        fbr3y = 0.0
     
     if drb == 0:
         fbr4x = 0.0
         fbr4y = 0.0
     else:
         fbr4 = mr*y_b_d2*c_d0/4.0*p_d0
-        fbr4x = fbr4*dxb/drb
-        fbr4y = fbr4*dyb/drb
+        fbr4x = fbr4
+        fbr4y = 0.0
 
     fbx = fbr1x + fbr2x + fbr3x + fbr4x
 
@@ -257,12 +258,12 @@ def fbfixed(mr, ayg, axg):
     arg = sqrt(pow(axg, 2.0) + pow(ayg, 2.0)) #CB
     fbr = 0.5*mr*arg
 
-    if arg == 0:
+    if fbr == 0:
         fbx = 0.0
         fby = 0.0
     else:
-        fbx = fbr*axg/arg
-        fby = fbr*ayg/arg
+        fbx = fbr
+        fby = 0.0
 
     return fbx, fby
 
@@ -301,7 +302,6 @@ def stat1(fabx, faby, fs2x, fs2y):
         rolling_state = True
     return rolling_state, fabx, faby
 
-
 def stat0(ddxb, ddyb, fs2x, fs2y):
     wd = fs2x*ddxb + fs2y*ddyb
     if wd >= 0:
@@ -309,7 +309,6 @@ def stat0(ddxb, ddyb, fs2x, fs2y):
     else:
         rolling_state = False
     return rolling_state
-
 
 def mu_val(iso, ub):
     L0 = iso.alpha0*iso.mu0
@@ -326,14 +325,14 @@ def mu_val(iso, ub):
         mu = iso.alpha1*iso.mu0
 
     if abs(ub) >= iso.umax and iso.typevf != 0:
-        mu = 100
+        mu = 100.0
     return mu
 
 #@profile 
-def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, smy, sky, cdy, iso, screen_on):
-  
+def simulator_osbix(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, smy, sky, cdy, iso, screen_on):
+    
     # INITIALIZATION OF PRIMARY INPUT VARIABLES
-    nit = 10
+    nit = 5
     nst = ndof - 1
     gamma = 0.5
     beta = 1/6
@@ -373,18 +372,7 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
     es = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
     ei = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
 
-    roll = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    theta_0 = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    theta_r = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    theta_r_dot2 = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    zbd = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    zbd_dot2 = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    Fs1x = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    Fs1y = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    Fs2x = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    Fs2y = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    Fbx = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
-    Fby = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
+    roll = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C #del
 
     error = np.zeros((ndt, 1), dtype=np.dtype('d'), order ='F') #C
 
@@ -488,7 +476,6 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
 
     rolling_state = False # INITIAL ROLLING STATE
 
-    
     for i in range(1, len(xg)):
 
         t += dt
@@ -511,25 +498,26 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
             dy2[0:nst, 0] = dy1[0:nst, 0] + ddy[0:nst, 0]
             dvy[0:nst, 0] = (gamma/beta/dt)*ddy[0:nst, 0] - gamma/beta*vy1[0:nst, 0] + dt*(1.0 - gamma/2.0/beta)*ay1[0:nst, 0]
             vy2[0:nst, 0] = vy1[0:nst, 0] + dvy[0:nst, 0]
-            
-            drb = sqrt(pow(dy2[ndof-1, 0], 2.0) + pow(dx2[ndof-1, 0], 2.0))
-            if drb > iso.umax:
-                print(prRed('WARNING: ') + prCyan('Isolator has crossed maximum displacement.'))
-                print(drb, dy2[ndof-1, 0], dx2[ndof-1, 0])
-            
-            theta_d02 = np.interp(drb, iso.xbtab, iso.ttab)
+                     
+            theta_d02 = np.interp(abs(dx2[ndof-1, 0]), iso.xbtab, iso.ttab)
+
+            if dx2[ndof-1, 0] == 0.0:
+                theta_d02 = 0.0
+            else:
+                theta_d02 = dx2[ndof-1, 0]/abs(dx2[ndof-1, 0])*theta_d02
+                
             theta_r_d02 = atan((iso.b0/iso.a0)*tan(theta_d02))
             c_d02 = iso.a0*sin(theta_d02)*cos(theta_r_d02) - iso.b0*cos(theta_d02)*sin(theta_r_d02)
             p_d02 = iso.a0*sin(theta_d02)*sin(theta_r_d02) + iso.b0*cos(theta_d02)*cos(theta_r_d02) 
             
-            fs1x, fs1y = fs1fixed(tm, rMrM, c_d02, p_d02, drb, dy2[ndof-1, 0], dx2[ndof-1, 0]) #CB
-
-            fbx, fby = fbfixed(iso.Mr, yg[i], xg[i]) #CB
+            fs1x, fs1y = fs1fixed(tm, rMrM, c_d02, p_d02, dx2[ndof-1, 0], dy2[ndof-1, 0], dx2[ndof-1, 0])
+    
+            fbx, fby = fbfixed(iso.Mr, yg[i], xg[i])
 
             fabx = (-1.0*cdx[nst, nst-1])*vx2[nst-1,0] + (-1.0*skx[nst, nst-1])*dx2[nst-1,0] - skx[ndof-1, ndof-1]*dx2[ndof-1,0] + px2[ndof-1] - fs1x - fbx
             faby = (-1.0*cdy[nst, nst-1])*vy2[nst-1,0] + (-1.0*sky[nst, nst-1])*dy2[nst-1,0] - sky[ndof-1, ndof-1]*dy2[ndof-1,0] + py2[ndof-1] - fs1y - fby
 
-            mu = mu_val(iso, drb)  #CB
+            mu = mu_val(iso, abs(dx2[ndof-1, 0]))
 
             fs2x, fs2y = fs2fixed(mu, tm, rMrM) #CB
 
@@ -544,8 +532,8 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
                 epx = px2 - np.dot(cdx, vx2) - np.dot(skx, dx2)
                 epy = py2 - np.dot(cdy, vy2) - np.dot(sky, dy2)
 
-                epx[ndof-1,0] = epx[ndof-1,0] - fabx - fs1x - fbx    
-                epy[ndof-1,0] = epy[ndof-1,0] - faby - fs1y - fby  
+                epx[ndof-1,0] = epx[ndof-1,0] - fs2x - fs1x - fbx    
+                epy[ndof-1,0] = epy[ndof-1,0] - fs2y - fs1y - fby  
 
                 ax2 = np.dot(smx_inv, epx)
                 ay2 = np.dot(smy_inv, epy)
@@ -553,10 +541,10 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
             dx1, vx1, px1, ax1 = dx2, vx2, px2, ax2 
             dy1, vy1, py1, ay1 = dy2, vy2, py2, ay2
             yd1, yv1, ya1 = yd2, yv2, ya2
-            theta_r_d01, theta_r_dot11, theta_r_dot21 = theta_r_d02, theta_r_dot12, theta_r_dot22
+            theta_r_d01, theta_r_dot11, theta_r_dot21 = theta_r_d02, theta_r_dot12, theta_r_dot22 
 
         else:
-            
+
             dfbx = 0.0
             dfby = 0.0
 
@@ -596,48 +584,39 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
                     arb = sqrt(pow(ax2[ndof-1, 0], 2.0) + pow(ay2[ndof-1, 0], 2.0))
                     arg = sqrt(pow(xg[i], 2.0) + pow(yg[i], 2.0))
 
-                    theta_d02 = np.interp(drb, iso.xbtab, iso.ttab)
+                    theta_d02 = np.interp(dx2[ndof-1, 0], iso.xbtab, iso.ttab)
                     theta_r_d02 = atan((iso.b0/iso.a0)*tan(theta_d02))
-                    c_d02 = iso.a0*sin(theta_d02)*cos(theta_r_d02) - iso.b0*cos(theta_d02)*sin(theta_r_d02)
-                    p_d02 = iso.a0*sin(theta_d02)*sin(theta_r_d02) + iso.b0*cos(theta_d02)*cos(theta_r_d02)
+                    c_d0 = iso.a0*sin(theta_d02)*cos(theta_r_d02) - iso.b0*cos(theta_d02)*sin(theta_r_d02)
+                    p_d0 = iso.a0*sin(theta_d02)*sin(theta_r_d02) + iso.b0*cos(theta_d02)*cos(theta_r_d02)
                     
-                    # dyb_dt0 = 2.0*(p_d02 - iso.b0) - yd1
-                    # dyb_dt1 = (gamma/beta/dt)*dyb_dt0 - gamma/beta*yv1 + dt*(1.0 - gamma/2.0/beta)*ya1
-                    # dyb_dt2 = (1.0/beta/pow(dt, 2.0))*dyb_dt0 - (1.0/beta/dt)*yv1 - (1.0/2.0/beta)*ya1
-                    # yd2 = yd1 + dyb_dt0
-                    # yv2 = yv1 + dyb_dt1
-                    # ya2 = ya1 + dyb_dt2
-
-                    dyb_dt0 = 2.0*(p_d02 - iso.b0) - yd1
+                    dyb_dt0 = 2.0*(p_d0 - iso.b0) - yd1
+                    dyb_dt1 = (gamma/beta/dt)*dyb_dt0 - gamma/beta*yv1 + dt*(1.0 - gamma/2.0/beta)*ya1
+                    dyb_dt2 = (1.0/beta/pow(dt, 2.0))*dyb_dt0 - (1.0/beta/dt)*yv1 - (1.0/2.0/beta)*ya1
                     yd2 = yd1 + dyb_dt0
-                    yv2 = (yd2 - yd1)/dt
-                    ya2 = (yv2 - yv1)/dt
-
-                    # dtheta_r_d0_dt0 = theta_r_d02 - theta_r_d01
-                    # dtheta_r_d0_dt1 = (gamma/beta/dt)*dtheta_r_d0_dt0 - gamma/beta*theta_r_dot11 + dt*(1.0 - gamma/2.0/beta)*theta_r_dot21
-                    # dtheta_r_d0_dt2 = (1.0/beta/pow(dt, 2.0))*dtheta_r_d0_dt0 - (1.0/beta/dt)*theta_r_dot11 - (1.0/2.0/beta)*theta_r_dot21
-                    # theta_r_d02 = theta_r_d01 + dtheta_r_d0_dt0
-                    # theta_r_dot12 = theta_r_dot11 + dtheta_r_d0_dt1
-                    # theta_r_dot22 = theta_r_dot21 + dtheta_r_d0_dt2
-
-                    theta_r_dot12 = (theta_r_d02 - theta_r_d01)/dt
-                    theta_r_dot22 = (theta_r_dot12 - theta_r_dot11)/dt
+                    yv2 = yv1 + dyb_dt1
+                    ya2 = ya1 + dyb_dt2
                     
-                    fs1x2, fs1y2 = fs1(tm, rMrM, ya2, c_d02, p_d02, drb, dy2[ndof-1, 0], dx2[ndof-1, 0])
+                    dtheta_r_d0_dt0 = theta_r_d02 - theta_r_d01
+                    dtheta_r_d0_dt1 = (gamma/beta/dt)*dtheta_r_d0_dt0 - gamma/beta*theta_r_dot11 + dt*(1.0 - gamma/2.0/beta)*theta_r_dot21
+                    dtheta_r_d0_dt2 = (1.0/beta/pow(dt, 2.0))*dtheta_r_d0_dt0 - (1.0/beta/dt)*theta_r_dot11 - (1.0/2.0/beta)*theta_r_dot21
+                    theta_r_d02 = theta_r_d01 + dtheta_r_d0_dt0
+                    theta_r_dot12 = theta_r_dot11 + dtheta_r_d0_dt1
+                    theta_r_dot22 = theta_r_dot21 + dtheta_r_d0_dt2
                     
+                    fs1x2, fs1y2 = fs1(tm, rMrM, ya2, c_d0, p_d0, drb, dy2[ndof-1, 0], dx2[ndof-1, 0])
                     mu = mu_val(iso, drb)
                     fs2x2, fs2y2 = fs2(tm, rMrM, mu, ya2)
-                    fbx2, fby2 = fb(iso.Jr, iso.Mr, drb, dy2[ndof-1, 0], dx2[ndof-1, 0], vrb, vy2[ndof-1, 0], vx2[ndof-1, 0], arg, yg[i], xg[i], arb, ay2[ndof-1, 0], ax2[ndof-1, 0], p_d02, theta_r_dot22, c_d02, ya2)
+                    fbx2, fby2 = fb(iso.Jr, iso.Mr, drb, dy2[ndof-1, 0], dx2[ndof-1, 0], vrb, vy2[ndof-1, 0], vx2[ndof-1, 0], arg, yg[i], xg[i], arb, ay2[ndof-1, 0], ax2[ndof-1, 0], p_d0, theta_r_dot22, c_d0, ya2)
 
                     dfs1x = fs1x2 - fs1x
-                    dfabx = fs2x2*vx2[ndof-1, 0]/vrb - fabx
+                    dfabx = fs2x2*vx[ndof-1, 0]/vrb - fabx
                     dfbx = fbx2 - fbx
 
                     dfs1y = fs1y2 - fs1y
-                    dfaby = abs(fs2y2)*vy2[ndof-1, 0]/vrb - faby
+                    dfaby = fs2y2*vy[ndof-1, 0]/vrb - faby
                     dfby = fby2 - fby
-            
-            # print('\n')
+
+                
             if nit > 1:
 
                 fbx = fbx + dfbx
@@ -649,7 +628,7 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
                 fabx = fabx + dfabx
                 faby = faby + dfaby
             
-            rolling_state = stat0(ddx[ndof-1], ddy[ndof-1], fabx, faby)
+            rolling_state = stat0(ddx[ndof-1], ddy[ndof-1], fs2x, fs2y)
 
             if rolling_state == False:
                 idx = 21 # del
@@ -710,25 +689,14 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
             for j in range(nst):
                 fx[index, j] = 1.0*np.dot(smx_diag[0:j+1].T, aax[0:j+1,index])   
                 fy[index, j] = 1.0*np.dot(smy_diag[0:j+1].T, aay[0:j+1,index])    
-            fx[index, ndof-1] = fabx
-            fy[index, ndof-1] = faby
+            fx[index, ndof-1] = fs2x/(tm*9.81)# + fs1x + fbx
+            fy[index, ndof-1] = fs2y/(tm*9.81)# + fs1y + fby
             ek[index, 0] = eki
             ed[index, 0] = edi
             es[index, 0] = esi
             ei[index, 0] = eii
 
             roll[index, 0] = idx # del
-            theta_0[index, 0] = theta_d02
-            theta_r[index, 0] = theta_r_d02
-            theta_r_dot2[index, 0] = theta_r_dot22
-            zbd[index, 0] = yd2
-            zbd_dot2[index, 0] = ya2
-            Fs1x[index, 0] = fs1x
-            Fs1y[index, 0] = fs1y
-            Fs2x[index, 0] = fs2x 
-            Fs2y[index, 0] = fs2y 
-            Fbx[index, 0] = fbx 
-            Fby[index, 0] = fby 
 
             error[index, 0] = (edi + esi + eki - eii)/(abs(edi + esi) + eki + abs(eii))
             # print(rolling_state)
@@ -754,7 +722,7 @@ def simulator_osbi(ref, xg, yg, dt, ndiv, ndt, lxy, ijk, ndof, smx, skx, cdx, sm
         print("Peak Isolator Displacement in X-Direction: % 8.6f cm" %(peakbasedispx*100.0))
         print("Peak Isolator Displacement in Y-Direction: % 8.6f cm" %(peakbasedispy*100.0))
     
-    result = ResultFixedXY(ref, ijk, time.T, gx.T, dx.T, vx.T, ax.T, aax.T, gy.T, dy.T, vy.T, ay.T, aay.T, fx, fy, ek, ed, es, ei, error, smx, skx, cdx, smy, sky, cdy, roll, theta_0, theta_r, theta_r_dot2, zbd, zbd_dot2, Fs1x, Fs1y, Fs2x, Fs2y, Fbx, Fby)
+    result = ResultFixedXY(ref, ijk, time.T, gx.T, dx.T, vx.T, ax.T, aax.T, gy.T, dy.T, vy.T, ay.T, aay.T, fx, fy, ek, ed, es, ei, error, smx, skx, cdx, smy, sky, cdy, roll)
     model = ModelInfo(ndof)
 
     return result, model

mhps/postprocessor.py
@@ -30,7 +30,7 @@ def profile(fnc):
 
 
 class ResultFixedXY:
-    def __init__(self, eq_refi, ijki, timei, gxi, dxi, vxi, axi, aaxi, gyi, dyi, vyi, ayi, aayi, fxi, fyi, eki, edi, esi, eii, errori, smxi, skxi, cdxi, smyi, skyi, cdyi):
+    def __init__(self, eq_refi, ijki, timei, gxi, dxi, vxi, axi, aaxi, gyi, dyi, vyi, ayi, aayi, fxi, fyi, eki, edi, esi, eii, errori, smxi, skxi, cdxi, smyi, skyi, cdyi, roll = 0, theta_0 = 0.0, theta_r= 0.0, theta_r_dot2= 0.0, zbd= 0.0, zbd_dot2= 0.0, Fs1x= 0.0, Fs1y= 0.0, Fs2x= 0.0, Fs2y= 0.0, Fbx= 0.0, Fby= 0.0): # del
         self.eq_ref = eq_refi
         self.ijk = ijki
         self.time = timei
@@ -57,6 +57,18 @@ class ResultFixedXY:
         self.smy = smyi
         self.sky = skyi
         self.cdy = cdyi
+        self.roll = roll
+        self.theta_0 = theta_0
+        self.theta_r = theta_r
+        self.theta_r_dot2 = theta_r_dot2
+        self.zbd = zbd
+        self.zbd_dot2 = zbd_dot2
+        self.Fs1x = Fs1x
+        self.Fs1y = Fs1y
+        self.Fs2x = Fs2x
+        self.Fs2y = Fs2y
+        self.Fbx = Fbx
+        self.Fby = Fby
 
 class ModelInfo:
     def __init__(self, nsti):
@@ -115,6 +127,24 @@ def get_result(result, responsevariable, floorstart, floorend, peaktype, dirn):
     elif responsevariable == 'er':
         vector = result.error
         vectorhead = ['Relative Error']
+    elif responsevariable == 'ro':
+        vector = result.roll
+        vectorhead = ['ROLL']
+    elif responsevariable == 'sm':
+        vector1 = result.theta_0
+        vector2 = result.theta_r
+        vector3 = result.theta_r_dot2
+        vector4 = result.zbd
+        vector5 = result.zbd_dot2
+        vector6 = result.Fs1x
+        vector7 = result.Fs1y
+        vector8 = result.Fs2x
+        vector9 = result.Fs2y
+        vector10 = result.Fbx
+        vector11 = result.Fby
+        vector = np.hstack((vector1, vector2, vector3, vector4, vector5, vector6, vector7, vector8, vector9, vector10, vector11))
+        vectorhead = ['theta_0', 'theta_r', 'theta_r_dot2', 'zbd', 'zbd_dot2', 'Fs1x', 'Fs1y', 'Fs2x', 'Fs2y', 'Fbx', 'Fby']
+
 
     if peaktype == 1:
         responsevalues = np.absolute(vector).max(axis=0)